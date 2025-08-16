e9d77fd66555a06928d5eb7441cecc6cd8fbd10b
remvst <remvst@gmail.com>
2025-08-16 11:16:09 -0400

    Fix build not working on my laptop

--- install-ect.sh
+++ install-ect.sh
@@ -4,9 +4,9 @@ brew install cmake || sudo apt-get install -y cmake
 
 git clone --recursive https://github.com/fhanau/Efficient-Compression-Tool.git
 pushd Efficient-Compression-Tool
-    git reset --hard 9aabc2
+    git reset --hard 9aabc23
     mkdir -p build
     cd build
-    cmake ../src
+    cmake ../src -DCMAKE_POLICY_VERSION_MINIMUM=3.5
     make
 popd
