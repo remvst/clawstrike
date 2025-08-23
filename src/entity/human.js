6c4c9b4a3d076a41a4d892e0b99cc7c6959fb618
remvst <remvst@gmail.com>
2025-08-22 21:48:03 -0400

    Bring back human shots

--- src/entity/human.js
+++ src/entity/human.js
@@ -119,10 +119,9 @@ class Human extends Entity {
         }
 
         if ((this.nextShot -= elapsed) <= 0 && this.seesCat) {
-            const bullet = new Bullet(this);
+            const bullet = this.world.addEntity(new Bullet(this));
             bullet.x = this.x + this.facing * 10 + Math.cos(this.aim) * 20;
             bullet.y = this.y - 20 + Math.sin(this.aim) * 20;
-            // this.world.addEntity(bullet);
             this.nextShot = 0.2;
         }
     }
